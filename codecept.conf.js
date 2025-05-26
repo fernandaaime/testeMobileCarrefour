c31f985b79dfa72a39bf579952ce83b6c99e0a66
Fernanda Aime <fe.aime23@gmail.com>
2025-05-26 17:59:35 -0300

    Ajustando login

--- codecept.conf.js
+++ codecept.conf.js
@@ -2,31 +2,32 @@ require('./hooks/clearAppCache');
 
 exports.config = {
   tests: './tests/*_test.js',
- 
-  timeout: 10,
+  timeout: 10, // Ajustado para segundos, conforme a sugestão do CodeceptJS
   output: './output',
   helpers: {
     Appium: {
       host: 'hub-cloud.browserstack.com',
-      port: 443, // Revertido para a porta padrão HTTPS do BrowserStack
+      port: 443, // Porta padrão HTTPS do BrowserStack
       user: process.env.BROWSERSTACK_USER,
       key: process.env.BROWSERSTACK_KEY,
-      restart: false, // Tenta NÃO reiniciar o navegador/aplicativo entre os cenários
-      keepBrowserState: true, // Mantém o estado do navegador/aplicativo entre os cenários
-      keepCookies: true, // Mantém cookies entre os cenários (se aplicável)
-      waitForTimeout: 15000,
-      desibstack:optionss: {
+      restart: false,
+      keepBrowserState: true,
+      keepCookies: true,
+      waitForTimeout: 15000, // Tempo de espera para elementos em milissegundos
+
+      desiredCapabilities: { // <-- ESSA LINHA FOI CORRIGIDA
         platformName: 'Android',
         'bstack:options': {
-          deviceName: 'Samsung Galaxy S22 Ultra', 
+          deviceName: 'Samsung Galaxy S22 Ultra',
           app: 'bs://293687243fde616dc5b2189c030d964bc89f71d2',
           automationName: 'UiAutomator2',
           appWaitActivity: '*',
+
         },
       },
 
       webdriverio: {
-        version: '8.18.2' // Versão do webdriverio que o CodeceptJS deve usar
+        version: '8.18.2'
       },
     }
   },
