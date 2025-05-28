e749739610877c96f51199fac9e92c1cf2bfe2e0
Fernanda Aime <fe.aime23@gmail.com>
2025-05-28 01:40:16 -0300

    Alteração 1

--- tests/login_test.js
+++ tests/login_test.js
@@ -114,7 +114,7 @@ Scenario('Navegação entre abas', async () => {
 });
 
 Scenario('Funcionalidade Scroll Horizontal', async () => {
-  await I.waitForElement('//android.widget.ScrollView', 10); // Aguarda o ScrollView visível antes do scroll
+  await I.scrollTo('//android.widget.ScrollView');
   await ScrollPage.realizarScrollHorizontal(5);
 });
 
